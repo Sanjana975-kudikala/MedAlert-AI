4c2c7d1903f618993f2d560ba9a9d1d71a9837dd
Sanjana Kudikala <sanjanakudikala@gmail.com>
2026-03-25 21:36:01 +0530

    Your commit message

--- app.py
+++ app.py
@@ -354,5 +354,6 @@ def history():
 @app.template_filter('markdown')
 def markdown_filter(text):
     return markdown.markdown(text)
-if __name__ == "__main__": 
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
